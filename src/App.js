9f6980be90b50cef35ab3d215f890e296eff9afd
YutakaYamasaki <uminchuvirtual@gmail.com>
2018-10-07 23:18:34 +0800

    Title chenge

--- src/App.js
+++ src/App.js
@@ -2,27 +2,24 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Title from './components/Tiltle';
+
+// ファイルの実質上の中身
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+ render() {
+   return (
+     <div className="App">
+       <header className="App-header">
+         <img src={logo} className="App-logo" alt="logo" />
+         <Title />
+       </header>
+       <p className="App-intro">
+         To get started, edit <code>src/App.js</code> and save to reload.
+       </p>
+     </div>
+   );
+ }
 }
 
+// エクスポート
 export default App;
