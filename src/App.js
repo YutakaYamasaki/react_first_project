efd9801d63d695333a4eaeea8d28a33fef9dcd52
YutakaYamasaki <uminchuvirtual@gmail.com>
2018-10-07 23:58:33 +0800

    props_try

--- src/App.js
+++ src/App.js
@@ -2,16 +2,28 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import Title from './components/Tiltle';
+import Title from './components/Title';
 
 // ファイルの実質上の中身
 class App extends Component {
+  onClickHandler = () => {
+    let title = document.getElementById('versionCounter');
+    let upgradeButton = document.getElementById('upgradeButton');
+    title.textContent = "4.0";
+    upgradeButton.style.display = "none";
+  }
  render() {
    return (
      <div className="App">
        <header className="App-header">
          <img src={logo} className="App-logo" alt="logo" />
-         <Title />
+         <Title 
+         title="Hello World 3.0"
+         titleStyle={{color: 'orange'}}
+         onClick ={this.onClickHandler}
+         >
+         Hello World <span id="versionCounter" style={{borderBottom: '1px solid orange'}}>3.0</span>
+         </Title>
        </header>
        <p className="App-intro">
          To get started, edit <code>src/App.js</code> and save to reload.
